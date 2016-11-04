ba22810b2c88542604f8cf68e6947d1a0fc1d760
Pablitto007 <pawkonik@gmail.com>
2016-11-04 22:06:24 +0100

    Update JdbcDetailsService.java

--- src/main/java/com/rentacar/mvc/service/impl/JdbcDetailsService.java
+++ src/main/java/com/rentacar/mvc/service/impl/JdbcDetailsService.java
@@ -8,20 +8,15 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.rentacar.mvc.doamin.Customer;
 import com.rentacar.mvc.service.CustomerService;
 
-@Service
+
 public class JdbcDetailsService extends JdbcDaoImpl {
 	
-	private CustomerService customerService;
-	
 	@Autowired
-	public JdbcDetailsService(CustomerService customerService){
-		this.customerService = customerService;
-	}
+	private CustomerService customerService;
 	
 	@Override
 	@Transactional(readOnly = true)
