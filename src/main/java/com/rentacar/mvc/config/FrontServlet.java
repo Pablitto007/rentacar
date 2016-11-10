871f527a675d9095e1a4a585035b2318442e388c
Pablitto007 <pawkonik@gmail.com>
2016-11-10 11:21:16 +0100

    Add FrontServlet in java configuration

---
+++ src/main/java/com/rentacar/mvc/config/FrontServlet.java
@@ -0,0 +1,59 @@
+package com.rentacar.mvc.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Equivalent for XML configuration in /webcontext/FrontServlet-context.xml
+ * I wrote both XML and Java class for better understanding Spring mixed configurations rules
+ */
+@Configuration
+@EnableWebMvc
+public class FrontServlet extends WebMvcConfigurerAdapter{
+	
+	@Bean
+	public Validator validator(){
+		LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
+		validator.setValidationMessageSource(messageSource());
+		return validator;
+	}
+	
+	@Bean
+	public MessageSource messageSource(){
+		ResourceBundleMessageSource messageSource = 
+				new ResourceBundleMessageSource();
+		messageSource.setBasename("messages_pl");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
+	
+	
+	@Bean
+	public CommonsMultipartResolver multiPartResolver() {
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+		multipartResolver.setMaxUploadSizePerFile(10240000);
+		return multipartResolver;
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	    registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+	
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry){
+		registry.jsp("/WEB-INF/views/", ".jsp");
+	}
+	
+
+
+}
